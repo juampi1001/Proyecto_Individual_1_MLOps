9ded3f898c3ca2e7b229bc53492e1bf204c155a9
juampi1001 <123843228+juampi1001@users.noreply.github.com>
2024-03-04 00:31:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+import traceback  
+from typing import List, Dict
+import pandas as pd
+from Funciones import Developer
 
 app = FastAPI()
 
@@ -16,7 +21,34 @@ async def root():
     """
     return {"Mensaje": "Proyecto Individual N° 1 - Juan Pablo Vitalevi"}
 
+# endpoint 1 - developer
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/Developer/{desarrolladora}", tags=['Developer'])
+async def endpoint1(desarrolladora: str):
+    """
+    Descripción: Retorna la cantidad de items y porcentaje de contenido Free por año 
+    según empresa desarrolladora
+    
+    Parámetros:
+        - desarrolladora (str): desarrolladora para la cual se 
+        - busca la cantidad de items y porcentaje free por anio. Debe ser un string, ejemplo: valve
+    
+    """
+    try:
+        # Validación adicional para asegurarse de que desarrolladora no sea nulo o esté vacío
+        if not genero or not genero.strip():
+            raise HTTPException(status_code=422, detail="El parámetro 'desarrolladora' no puede ser nulo o estar vacío.")
+
+        result = Developer(desarrolladora)
+
+            # Validación para verificar si el género existe en los datos
+        if not result:
+            raise HTTPException(status_code=404, detail=f"No se encontró información para la desarrolladora '{desarrolladora}'.")
+
+        return result
+    
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=500, detail=f"Error al cargar el archivo developer.parquet: {str(e)}")
+    except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail=f"Error interno del servidor: {str(e)}")
