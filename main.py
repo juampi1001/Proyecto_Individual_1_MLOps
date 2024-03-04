e957c32a92b511f85a87503660d45231a63e0415
juampi1001 <123843228+juampi1001@users.noreply.github.com>
2024-03-03 23:58:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    """
+    Proyecto FastAPI - Sistema de Recomendaciones
+
+    Versión: 1.0.0
+
+    ---
+
+    """
+    return {"Mensaje": "Proyecto Individual N° 1 - Juan Pablo Vitalevi"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
