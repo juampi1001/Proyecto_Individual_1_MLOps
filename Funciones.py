9a8f846a356ce4ffc44f82552a4400528813c6b7
juampi1001 <vitalevijuampi105@gmail.com>
2024-03-05 20:26:36 -0300

    commit

--- Funciones.py
+++ Funciones.py
@@ -19,31 +19,30 @@ import os
 # Input: **Developer.parquet**
 
 def Developer(desarrolladora: str):
-    
     df = pd.read_parquet('Datasets/archivos_API/Developer.parquet')
+    
     # Filtrar el DataFrame por la desarrolladora específica
     df_desarrolladora = df[df['developer'] == desarrolladora.lower()]
+    
+    if df_desarrolladora.empty:
+        raise ValueError(f"No hay datos disponibles para la desarrolladora '{desarrolladora}'.")
     
     # Eliminar filas con valores nulos en la columna 'release_year'
     df_desarrolladora = df_desarrolladora.dropna(subset=['release_year'])
     
-    # Contar la cantidad de ítems por año
     cantidad_items_por_anio = df_desarrolladora.groupby('release_year').size()
     
-    # Filtrar juegos gratuitos
     df_free = df_desarrolladora[df_desarrolladora['price'] == 0]
     
-    # Calcular el porcentaje de contenido gratuito por año
-    porcentaje_free_por_anio = df_free.groupby('release_year').size() / cantidad_items_por_anio * 100
-    porcentaje_free_por_anio = porcentaje_free_por_anio.fillna(0).astype(int).astype(str) + "%"
+    porcentaje_free_por_anio = (df_free.groupby('release_year').size() / cantidad_items_por_anio * 100).fillna(0).astype(int).astype(str) + "%"
     
-    # Unir los resultados en un DataFrame
     resultados = pd.DataFrame({
         'Cantidad de Items': cantidad_items_por_anio,
         'Contenido Free': porcentaje_free_por_anio
     })
     
     return resultados
+
 
 
     # ### Endpoint 2
